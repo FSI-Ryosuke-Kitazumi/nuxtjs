e03657fad096ad51721092fa2bb75fc5e3d51ad5
FSI-Ryosuke-Kitazumi <kitazumr@fsi.co.jp>
2024-09-24 07:29:11 +0000

    add: eslint, prettier, vuetify delete tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -11,14 +13,31 @@ export default defineNuxtConfig({
   },
   postcss: {
     plugins: {
-      tailwindcss: {},
       autoprefixer: {},
     },
   },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   modules: [
+    '@nuxt/eslint',
     "@pinia/nuxt",
     "@nuxt/test-utils/module",
     "@hebilicious/vue-query-nuxt",
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    
   ],
   pinia: {
     storesDirs: ["./stores/**"],
