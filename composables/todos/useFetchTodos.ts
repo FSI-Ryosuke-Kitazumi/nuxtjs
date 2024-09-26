82c19b8c16792e5e93b9fc5fb34d4bd312215638
FSI-Ryosuke-Kitazumi <kitazumr@fsi.co.jp>
2024-09-26 06:39:55 +0000

    update rule

--- composables/todos/useFetchTodos.ts
+++ composables/todos/useFetchTodos.ts
@@ -1,5 +1,6 @@
 import type { Todo } from "~/types/api/todos";
 
+// RULE: useXXXのような関数を使っている、refなどのvueの機能を使っている場合、ファイル名・関数名に「use」プレフィックスをつける
 export function useFetchTodos() {
   const url = "/todos";
 
